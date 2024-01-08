2d5138b571960906bb92ec06dbf25d945dde70e2
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-01-08 12:40:55 +0900

    Feat: 추가 AddLinkBar.tsx

---
+++ src/components/AddLinkBar.tsx
@@ -0,0 +1,112 @@
+import styled from 'styled-components';
+import linkIcon from '../assets/ico-link.png';
+import { ChangeEvent, useContext, useState } from 'react';
+import modalContext from '../components/modal/modalContext';
+
+const AddLinkBarContainer = styled.div`
+  padding: 6rem 0 9rem 0;
+  background-color: var(--bg);
+
+  @media screen and (max-width: 1124px) {
+    padding: 6rem 3.2rem 9rem 3.2rem;
+  }
+
+  @media screen and (min-width: 375px) and (max-width: 768px) {
+    padding: 2.4rem 3.2rem 4rem 3.2rem;
+  }
+`;
+
+const Wrap = styled.div`
+  position: relative;
+  width: 80rem;
+  margin: 0 auto;
+
+  @media screen and (max-width: 1124px) {
+    width: 100%;
+  }
+
+  &::before {
+    content: '';
+    display: block;
+    position: absolute;
+    left: 2rem;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 20px;
+    height: 20px;
+    background: url('${linkIcon}') no-repeat;
+
+    @media screen and (min-width: 375px) and (max-width: 768px) {
+      left: 1rem;
+    }
+  }
+`;
+
+const Input = styled.input`
+  width: 80rem;
+  padding: 2.2rem 5.2rem;
+  border-radius: 15px;
+  border: 1px solid var(--primary);
+  color: var(--gray60);
+  font-size: 1.6rem;
+  outline: none;
+
+  @media screen and (max-width: 1124px) {
+    width: 100%;
+  }
+
+  @media screen and (min-width: 375px) and (max-width: 768px) {
+    padding: 1.8rem 3.4rem;
+    font-size: 1.4rem;
+  }
+`;
+
+const Button = styled.button`
+  border: none;
+  position: absolute;
+  right: 2rem;
+  top: 50%;
+  transform: translateY(-50%);
+  display: block;
+  width: 8rem;
+  height: 3.7rem;
+  cursor: pointer;
+  background-image: linear-gradient(135deg, #6d6afe 0%, #6ae3fe 100%);
+  border-radius: 0.8rem;
+  color: var(--gray-f5f5);
+  font-size: 1.4rem;
+  font-weight: 600;
+  text-align: center;
+  line-height: 3.7rem;
+
+  @media screen and (min-width: 375px) and (max-width: 768px) {
+    right: 1rem;
+  }
+`;
+
+function AddLinkBar() {
+  const { handleClickModalOpen } = useContext(modalContext);
+  const [linkValue, setLinkValue] = useState<string>('');
+
+  const handleChangeLinkValue = (e: ChangeEvent<HTMLInputElement>) => setLinkValue(e.target.value);
+
+  return (
+    <>
+      <AddLinkBarContainer>
+        <Wrap>
+          <Input
+            type="text"
+            placeholder="링크를 추가해보세요"
+            onChange={handleChangeLinkValue}
+            value={linkValue}
+          />
+          <Button type="button" onClick={() => linkValue && handleClickModalOpen('folderAdd')}>
+            추가하기
+          </Button>
+        </Wrap>
+      </AddLinkBarContainer>
+    </>
+  );
+}
+
+export default AddLinkBar;
