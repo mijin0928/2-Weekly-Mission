45e1b0558443ac6ed4cf6827ca9cce790ea099ca
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-01-08 09:36:16 +0900

    Feat: 추가 UserButton.ts

---
+++ src/components/UserButto.tsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+import { useLocation } from 'react-router-dom';
+
+interface UserButtonProps {
+  handleClickLogin: () => void;
+  handleClickEmailCheck: () => void;
+}
+
+const ButtonContainer = styled.div`
+  margin: 3rem 0 0;
+`;
+
+const Button = styled.button`
+  display: block;
+  width: 100%;
+  padding: 1.6rem 2rem;
+  border: 0;
+  border-radius: 0.8rem;
+  text-align: center;
+  font-size: 1.8rem;
+  font-weight: 600;
+  line-height: 2.1rem;
+  cursor: pointer;
+  color: var(--gray-f5f5);
+  background: linear-gradient(90.99deg, #6d6afe 0.12%, #6ae3fe 101.84%);
+`;
+
+function UserButton({ handleClickLogin, handleClickEmailCheck }: UserButtonProps) {
+  const { pathname } = useLocation();
+  return (
+    <ButtonContainer>
+      <Button type="button" onClick={pathname === '/signup' ? handleClickEmailCheck : handleClickLogin}>{pathname === '/signup' ? '회원가입' : '로그인'}</Button>
+    </ButtonContainer>
+  );
+}
+
+export default UserButton;
+
+
