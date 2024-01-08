3498d697e894bcc0d5a2790abb58aad41c677967
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-01-08 15:13:29 +0900

    Feat: 추가 shareKakao.ts

---
+++ src/components/shareKakao.ts
@@ -0,0 +1,32 @@
+export const shareKakao = ({ title }: {title: string}, currentUrl: string) => {
+  if ((window as any).Kakao) {
+    const kakao: any = (window as any).Kakao;
+    
+    if (!kakao.isInitialized()) {
+      kakao.init('abbd86379133e623416676a637862b22');
+    }
+
+    kakao.Link.sendDefault({
+      objectType: "feed", 
+      content: {
+        title: `${title}`,
+        description: `${title} 폴더 링크 목록`,
+        imageUrl:
+          "https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png",
+        link: {
+          mobileWebUrl: `${currentUrl}`,
+          webUrl: `${currentUrl}`,
+        },
+      },
+      buttons: [
+        {
+          title: "자세히 보러 가기",
+          link: {
+            mobileWebUrl: `${currentUrl}`,
+            webUrl: `${currentUrl}`,
+          },
+        },
+      ],
+    });
+  }
+};
