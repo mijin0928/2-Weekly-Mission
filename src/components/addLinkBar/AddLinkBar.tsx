e5cf018f30677e29cf08f74b6703c6e95f6a801f
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-01-16 11:40:04 +0900

    Design:수정 AddLinkBar 스타일 컴포넌트  수정

--- src/components/addLinkBar/AddLinkBar.tsx
+++ src/components/addLinkBar/AddLinkBar.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { ChangeEvent, useContext, useState } from 'react';
+import { ChangeEvent, useContext, useState, useRef, useEffect } from 'react';
 import ModalContext from '@/src/components/modal/ModalContext';
 
 export default function AddLinkBar() {
@@ -9,33 +9,70 @@ export default function AddLinkBar() {
   const handleChangeLinkValue = (e: ChangeEvent<HTMLInputElement>) =>
     setLinkValue(e.target.value);
 
+  const addLinkBar = useRef<HTMLDivElement>(null);
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  const callback = (entries: any) => {
+    const [entry] = entries;
+    setIsVisible(entry.isIntersecting);
+  };
+
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 1.0,
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(callback, options);
+    if (addLinkBar.current) {
+      observer.observe(addLinkBar.current);
+    }
+
+    return () => {
+      if (addLinkBar.current) {
+        observer.unobserve(addLinkBar.current);
+      }
+    };
+  }, [addLinkBar, options]);
+
   return (
     <>
-      <AddLinkBarContainer>
-        <Wrap>
-          <Input
-            type="text"
-            placeholder="링크를 추가해보세요"
-            onChange={handleChangeLinkValue}
-            value={linkValue}
-          />
-          <Button
-            type="button"
-            onClick={() => linkValue && handleClickModalOpen('folderAdd')}
-          >
-            추가하기
-          </Button>
-        </Wrap>
+      <AddLinkBarContainer ref={addLinkBar} className={isVisible ? '' : 'active'}>
+        <Input
+          type="text"
+          placeholder="링크를 추가해보세요"
+          onChange={handleChangeLinkValue}
+          value={linkValue}
+        />
+        <Button
+          type="button"
+          onClick={() => linkValue && handleClickModalOpen('folderAdd')}
+        >
+          추가하기
+        </Button>
       </AddLinkBarContainer>
     </>
   );
 }
 
 const AddLinkBarContainer = styled.div`
+  position: relative;
+  width: 80rem;
+  margin: 0 auto;
   padding: 6rem 0 9rem 0;
+  background-color: var(--bg);
+
+  &.active {
+    position: fixed;
+    bottom: 0;
+    z-index: 1;
+    padding: 2.4rem 3.2rem;
+  }
 
   @media screen and (max-width: 1124px) {
     padding: 6rem 3.2rem 9rem 3.2rem;
+    width: 100%;
   }
 
   @media screen and (min-width: 375px) and (max-width: 768px) {
@@ -43,48 +80,20 @@ const AddLinkBarContainer = styled.div`
   }
 `;
 
-const Wrap = styled.div`
-  position: relative;
-  width: 80rem;
-  margin: 0 auto;
-
-  @media screen and (max-width: 1124px) {
-    width: 100%;
-  }
-
-  &::before {
-    content: '';
-    display: block;
-    position: absolute;
-    left: 2rem;
-    top: 50%;
-    transform: translateY(-50%);
-    width: 20px;
-    height: 20px;
-    background: url('/image/ico-link.png') no-repeat;
-
-    @media screen and (min-width: 375px) and (max-width: 768px) {
-      left: 1rem;
-    }
-  }
-`;
-
 const Input = styled.input`
-  width: 80rem;
+  width: 100%;
   padding: 2.2rem 5.2rem;
   border-radius: 15px;
   border: 1px solid var(--primary);
   color: var(--gray60);
   font-size: 1.6rem;
   outline: none;
-
-  @media screen and (max-width: 1124px) {
-    width: 100%;
-  }
+  background: url('/image/ico-link.png') no-repeat 2rem center var(--white);
 
   @media screen and (min-width: 375px) and (max-width: 768px) {
-    padding: 1.8rem 3.4rem;
+    padding: 1.8rem 4.4rem;
     font-size: 1.4rem;
+    background: url('/image/ico-link.png') no-repeat 1.6rem center var(--white);
   }
 `;
 
@@ -92,8 +101,7 @@ const Button = styled.button`
   border: none;
   position: absolute;
   right: 2rem;
-  top: 50%;
-  transform: translateY(-50%);
+  top: 7.5rem;
   display: block;
   width: 8rem;
   height: 3.7rem;
@@ -106,7 +114,11 @@ const Button = styled.button`
   text-align: center;
   line-height: 3.7rem;
 
+  @media screen and (max-width: 1124px) {
+    right: 5.2rem;
+  }
+
   @media screen and (min-width: 375px) and (max-width: 768px) {
-    right: 1rem;
+    top: 3.2rem;
   }
 `;
